d9ddbf0b1579da0e433a120fa9f91c96f096da9c
Forhad96 <forhadairdrop@gmail.com>
2024-03-12 19:45:12 +0600

    user sing up and image bb api

--- client/src/pages/RoomDetails/RoomDetails.jsx
+++ client/src/pages/RoomDetails/RoomDetails.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
-import Container from "../../components/Shared/Container";
 import { useParams } from "react-router-dom";
 import Loader from "../../components/Shared/Loader";
 import { Helmet } from "react-helmet-async";
+import Container from "../../components/Shared/Container";
+import Header from "../../components/RoomDetails/Heading";
+import RoomInfo from "../../components/RoomDetails/RoomInfo";
+import RoomReservation from "../../components/RoomDetails/RoomReservation";
+
 
 const RoomDetails = () => {
   const { id } = useParams();
@@ -11,7 +15,9 @@ const RoomDetails = () => {
 
   useEffect(() => {
     setLoading(true);
-    fetch("/rooms.json")
+    fetch(
+      "https://raw.githubusercontent.com/shakilahmedatik/stay-vista-resources/main/data/rooms.json"
+    )
       .then((res) => res.json())
       .then((data) => {
         const singleRoom = data.find((room) => room._id === id);
@@ -26,11 +32,21 @@ const RoomDetails = () => {
       <Helmet>
         <title>{room?.title}</title>
       </Helmet>
-      <div className="">
-        <div className="flex flex-col gap-6">{/* Header */} hd</div>
-        <div className="">{/* Room Info */}</div>
-        {/* Calender */}
-      </div>
+      {room && (
+        <div className="max-w-screen-lg mx-auto">
+          <div className="flex flex-col gap-6">
+            <Header room={room} />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <RoomInfo room={room} />
+
+            <div className="md:col-span-3 order-first md:order-last mb-10">
+              {/* RoomReservation */}
+              <RoomReservation room={room} />
+            </div>
+          </div>
+        </div>
+      )}
     </Container>
   );
 };
